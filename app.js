93c3b081a7a3426493d44f002840dc171e020c2b
lukabla <juve1736@gmail.com>
2023-08-29 11:54:56 +0400

    add countdown

--- app.js
+++ app.js
@@ -14,8 +14,8 @@ setInterval(() => {
 
 function mainsSlider() {
   const slide = document.querySelectorAll(".slide");
-  const next = document.querySelector(".next");
-  const prev = document.querySelector(".prev");
+  // const next = document.querySelector(".next");
+  // const prev = document.querySelector(".prev");
 
   let activeIndex = 0;
 
@@ -46,18 +46,19 @@ function mainsSlider() {
     }
     renderSlides();
   }
-  prev.addEventListener("click", showPrev);
-  next.addEventListener("click", showNext);
+  // prev.addEventListener("click", showPrev);
+  // next.addEventListener("click", showNext);
 
   let setIntervalId = null;
 
   function startAutoSlider() {
-    setIntervalId = setInterval(showNext, 1000);
+    const setIntervalId = setInterval(showNext, 5000);
   }
   startAutoSlider();
   const sliderWraper = document.querySelector(".slider-wraper");
 
-  sliderWraper.addEventListener("mouseenter", () => {});
+  sliderWraper.addEventListener("mouseleave", startAutoSlider);
+  // sliderWraper.addEventListener("mouseenter", clearInterval(se));
 
   //   startAutoSlider();
 
@@ -72,3 +73,27 @@ function mainsSlider() {
   });
 }
 mainsSlider();
+
+const countDownDate = new Date("Jan 1, 2024 00:00:00").getTime();
+const x = setInterval(function () {
+  const now = new Date().getTime();
+  const distance = countDownDate - now;
+
+  let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("days").innerHTML = days;
+  document.getElementById("hours").innerHTML = hours;
+  document.getElementById("minutes").innerHTML = minutes;
+  document.getElementById("seconds").innerHTML = seconds;
+
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("days").innerHTML = "00";
+    document.getElementById("hours").innerHTML = "00";
+    document.getElementById("minutes").innerHTML = "00";
+    document.getElementById("seconds").innerHTML = "00";
+  }
+}, 1000);
